96810e82dc6271552b4c0a4c2e39751482e7be42
mscastellano <105226970+mscastellano@users.noreply.github.com>
2024-05-23 12:41:42 -0500

    style in section 1 - Kiara Carvajal

--- js/section1.js
+++ js/section1.js
@@ -1,10 +1,46 @@
-class IntroElement extends HTMLElement {
+class ElementoTitulo extends HTMLElement {
     constructor() {
         super();
         const shadow = this.attachShadow({ mode: 'open' });
         shadow.innerHTML = `
-          
+            <style>
+                .titulo {
+                    text-align: center;
+                    padding: 50px 20px;
+                }
+                .titulo h1 {
+                    font-size: 2em;
+                    color: #00b894;
+                }
+                .titulo p {
+                    font-size: 1.2em;
+                    margin: 20px 0;
+                    max-width: 600px; 
+                    line-height: 1.5;
+                    text-align: center;
+                    margin: 0 auto;
+
+                }
+                .titulo button {
+                    padding: 10px 20px;
+                    font-size: 1em;
+                    color: #fff;
+                    background-color: #00b894;
+                    border: none;
+                    cursor: pointer;
+                    margin-top: 20px;
+                }
+            </style>
+            <div class="titulo">
+                <h1><span style="color: #00b894;">Capturando Momentos</h1>
+                <container>
+                <row>
+                <p>Somos apasionados de la fotografía y estamos dedicados a capturar tus momentos más preciados con arte y profesionalismo. Navega por nuestra galería para ver nuestras obras y descubre cómo podemos transformar tus instantes especiales en recuerdos eternos. </p>
+                <button>GALERIA</button>
+                </row>
+                </container>
+            </div>
         `;
-    }
-}
-customElements.define('intro-element', IntroElement);
+        }
+    }
+customElements.define('elemento-titulo',ElementoTitulo);
